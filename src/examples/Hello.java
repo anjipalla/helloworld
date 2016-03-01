7c613c192ba9cdaa21cc0ff3129d58ed44011322
Martist <m.stepanek@email.cz>
2016-03-01 17:10:42 +0100

    Update Hello.java

--- src/examples/Hello.java
+++ src/examples/Hello.java
@@ -40,7 +40,7 @@ public final class Hello extends HttpServlet {
         writer.println("<table border=\"0\" cellpadding=\"10\">");
         writer.println("<tr>");
         writer.println("<td>");
-        writer.println("<img src=\"images/springsource.png\">");
+        writer.println("<img src=\"images/1.jpg\">");
         writer.println("</td>");
         writer.println("<td>");
         writer.println("<h1>Sample Application Servlet</h1>");
